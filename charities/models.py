5d1b2b91b9053633bbf004076e6810a97e25c7f8
Sarah Ghanei <sarahghanei1997@gmail.com>
2021-07-11 11:24:45 +0430

    final commit for Taskmanager of phase1

--- charities/models.py
+++ charities/models.py
@@ -40,7 +40,7 @@ class TaskManager(models.Manager):
         return self.filter(assigned_benefactor__user=user)
 
     def all_related_tasks_to_user(self, user):
-        return self.filter(Q(assigned_benefactor__user=user) | Q(charity__user=user) & Q(state="P"))
+        return self.filter(Q(assigned_benefactor__user=user) | Q(charity__user=user) | Q(state="P"))
 
 
 class Task(models.Model):
